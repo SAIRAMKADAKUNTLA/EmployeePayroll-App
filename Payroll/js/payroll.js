5b27cebf5d310d5669a9842c9f37fc4e41f4b263
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-07-19 22:27:14 +0530

    [Sairam]Added:Ability to validate the using the getters and setters

--- Payroll/js/payroll.js
+++ Payroll/js/payroll.js
@@ -3,4 +3,55 @@ const output=document.querySelector('.salary-output')
 output.textContent=salary.value
 salary.addEventListener('input',function(){
     output.textContent=salary.value
-})
+});
+class EmppayrollData{
+    get id(){return this._id}
+    set id(id){
+        this._id=this.id
+    }
+    get name(){
+        return this._name
+    }
+    set name(name){
+        let nameregex=RegExp("^[A-Z]{1}[A-Za-z]{2,}$")
+        if(nameregex.test(name))
+        this._name=name;
+        else throw "Name is incorrect"
+    }
+    get profilePic() {
+        return this._profilePic;
+    }
+    set profilePic(profilePic){
+        this._profilePic = profilePic;
+    }
+    get gender() {
+        return this._gender;
+    }
+    set gender(gender) {
+        this._gender = gender;
+    }
+    get department(){
+        return this._department;
+    }
+    set department(department) {
+        this._department = department;
+    }
+    get salary() {
+        return this._salary
+    }
+    set salary(salary) {
+        this._salary = salary;
+    }
+    get note() {
+        return this._note;
+    }
+    set note(note) {
+        this._note = note;
+    }
+    get startDate() {
+        return this._startDate;
+    }
+    set startDate(startDate) {
+        this._startDate = startDate;
+    }
+}
